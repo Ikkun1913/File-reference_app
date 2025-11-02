a25c27dfebf1cf21d6152d06bf2de9e0b289d0b2
Ik <Ikkun19_touhou@proton.me>
2025-11-02 16:49:15 +0900

    Changes.

--- src/main.py
+++ src/main.py
@@ -7,12 +7,7 @@ root = tk.Tk()
 root.title("File Reference App (by Ik)")
 
 #main
-def open_file():
-    filepath = filedialog.askopenfilename()
-
-# ボタンを作成
-btn_open = tk.Button(root, text="ファイルを開く", command=open_file)
-btn_open.pack(pady=10)
+filedialog.askopenfilename()
 
 #ここまでプログラム
 root.mainloop()
