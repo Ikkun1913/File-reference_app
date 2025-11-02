71cca1c9a6ba8570bcf501e26cda16e952e4061c
Ik <Ikkun19_touhou@proton.me>
2025-11-02 16:46:54 +0900

    Changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 #import,from
 import tkinter as tk
-import os
 from tkinter import filedialog
 
 #meta
@@ -11,16 +10,9 @@ root.title("File Reference App (by Ik)")
 def open_file():
     filepath = filedialog.askopenfilename()
 
-def open_folder():
-    iDir = os.path.abspath(os.path.dirname(__file__))
-    folder_name = tk.filedialog.askdirectory(initialdir=iDir)
-
 # ボタンを作成
 btn_open = tk.Button(root, text="ファイルを開く", command=open_file)
 btn_open.pack(pady=10)
-
-button = tk.Button(root, text="フォルダを開く", command=open_folder)
-button.pack(pady=10)
 
 #ここまでプログラム
 root.mainloop()
